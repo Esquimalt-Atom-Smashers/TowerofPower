c3e880e0ffd874a844a40814709f59d755d3740b
Zev G <vezvez1234@gmail.com>
2022-02-23 16:06:10 -0800

    Wrote javadoc for Event.java class

--- src/main/java/frc/robot/events/Event.java
+++ src/main/java/frc/robot/events/Event.java
@@ -2,7 +2,17 @@ package frc.robot.events;
 
 import frc.robot.Robot;
 
-// TODO javadoc comments for this class
+/**
+ * When specific events occur on the robot it will fire an event to all of that event's corresponding event handlers.
+ * The information for the corresponding event is contained in an Event object. Since different types of events contain
+ * different information there are other classes which extend this one to represent those events. These include: {@link DpadEvent}
+ * for events on the DPad, {@link SingleStickEvent} for when a single stick on the joystick is moved, and {@link ButtonEvent} for
+ * when a button on the joystick is pressed.
+ * @see Robot
+ * @see EventHandler
+ * @see Events
+ * @see Filters
+ */
 public abstract class Event {
     
     private final Robot robot;
