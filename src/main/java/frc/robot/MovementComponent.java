dcb887d3652293735cc57804b56205ceae8e2bf9
Zev G <vezvez1234@gmail.com>
2022-02-25 14:53:54 -0800

    Wrote documentation for MovementComponent.java

--- src/main/java/frc/robot/MovementComponent.java
+++ src/main/java/frc/robot/MovementComponent.java
@@ -2,23 +2,30 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
 
+/**
+ * Component to move the robot when the controller joysticks are moved.
+ */
 public class MovementComponent extends ComponentBase {
 
     /** Controls if we use the second joystick to rotate the robot. */
     private static final boolean ALTERNATE_ROTATE = true;
 
+    /** Creates and initialize the component with the given Robot.
+     * @see ComponentBase#ComponentBase(Robot)
+     */
     public MovementComponent(Robot robot) {
         super(robot);
     }
 
+    /**
+     * Moves the robot based on if the Robot's joystick is being moved. If {@link MovementComponent#ALTERNATE_ROTATE}
+     * is true then both joysticks on the controller are used to move the Robot. Otherwise, just one joystick is used.
+     * @see Robot#getJoystick()
+     */
     @Override
     public void teleopPeriodic() {
         Joystick stick = robot.getJoystick();
-        //  -stick.getY() gives an error:
-        //CTR: Firm Vers could not be retrieved. Use Phoenix Tuner to check ID and firmware(CRF) version.
-        robot.move(stick.getY(), ALTERNATE_ROTATE ? stick.getZ() : stick.getX()); //Wwhat is an HID?
-
-        
+        robot.move(stick.getY(), ALTERNATE_ROTATE ? stick.getZ() : stick.getX());
     }
-    
+
 }
