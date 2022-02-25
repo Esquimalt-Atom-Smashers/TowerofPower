0bcccc0bf68f4fe0d70c124d08bc6025b909d207
Zev G <vezvez1234@gmail.com>
2022-02-25 14:55:06 -0800

    Rewrote a lot of the documentation for ComponentBase.java

--- src/main/java/frc/robot/ComponentBase.java
+++ src/main/java/frc/robot/ComponentBase.java
@@ -19,31 +19,60 @@ public abstract class ComponentBase {
     protected final Robot robot;
 
     /**
-     *
-     * @param robot The robot which this will be attached to.
+     * Generates a new component and initializes it with a Robot.
+     * @param robot The robot which this component will be attached to.
      */
     public ComponentBase(Robot robot) {
         this.robot = robot;
     }
 
     /**
-     * @return The robot defined in the constructor.
+     * @return The robot associated with this robot.
      */
     public Robot getRobot() {
         return robot;
     }
 
     /**
-     * This method will get called by the Robot.java class whenever its teleopPeriodic methods is called.
+     * This method will get called by the associated Robot whenever its teleopPeriodic method is called.
+     * @see Robot#teleopPeriodic()
      */
     public void teleopPeriodic() {}
+    /**
+     * This method will get called by the associated Robot whenever its autonomousPeriodic method is called.
+     * @see Robot#autonomousPeriodic()
+     */
     public void autonomousPeriodic() {}
+    /**
+     * This method will get called by the associated Robot whenever its disabledPeriodic method is called.
+     * @see Robot#disabledPeriodic()
+     */
     public void disabledPeriodic() {}
+    /**
+     * This method will get called by the associated Robot whenever its testPeriodic method is called.
+     * @see Robot#testPeriodic()
+     */
     public void testPeriodic() {}
 
+    /**
+     * This method will get called by the associated Robot whenever its teleopInit method is called.
+     * @see Robot#teleopInit()
+     */
     public void teleopInit() {}
+    /**
+     * This method will get called by the associated Robot whenever its autonomousInit method is called.
+     * @see Robot#autonomousInit()
+     */
     public void autonomousInit() {}
+    /**
+     * This method will get called by the associated Robot whenever its disabledInit method is called.
+     * @see Robot#disabledInit()
+     */
     public void disabledInit() {}
+    /**
+     * This method will get called by the associated Robot whenever its testInit method is called.
+     * @see Robot#testInit()
+     */
     public void testInit() {}
 
     /**
@@ -54,5 +83,5 @@ public abstract class ComponentBase {
     public void attach() {
         robot.addComponent(this);
     }
-    
+
 }
